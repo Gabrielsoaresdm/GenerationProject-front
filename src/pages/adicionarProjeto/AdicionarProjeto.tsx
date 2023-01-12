f1fdf9f1101ca761952c90747b9f0200bb97275b
Gabriel Soares <112710457+Gabrielsoaresdm@users.noreply.github.com>
2023-01-11 21:33:54 -0300

    Add files via upload

---
+++ src/pages/adicionarProjeto/AdicionarProjeto.tsx
@@ -0,0 +1,10 @@
+function AdicionarProjeto() {
+    return (
+        <div>
+            <h1>AdicionarProjeto</h1>
+            <p>Conteúdo da página</p>
+        </div>
+    )
+}
+
+export default AdicionarProjeto;
